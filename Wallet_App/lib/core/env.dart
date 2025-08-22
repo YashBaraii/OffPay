3d32575ed3c255cccc316568f0db009af7af145b
yashbarai07 <yashbarai2308@gmail.com>
2025-08-22 22:20:08 +0530

    qr code generation

--- Wallet_App/lib/core/env.dart
+++
@@ -1,6 +0,0 @@
-class Env {
-  static const supabaseUrl =
-      String.fromEnvironment('SUPABASE_URL', defaultValue: '');
-  static const supabaseAnonKey =
-      String.fromEnvironment('SUPABASE_ANON_KEY', defaultValue: '');
-}
